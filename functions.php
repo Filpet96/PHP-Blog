08bf96992bb5a9a35e202c047b5a34182358efcf
Filpet96 <Fpetersson96@gmail.com>
2017-10-17 19:30:53 +0200

    Created foreach loop in html for all post divs and created sortByDate function in functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 declare(strict_types=1);
 
+require(__DIR__ . '/post.php');
+
+
+function sortByDate($posts, $posts1) {
+
+    return strtotime($posts['Published']) <  strtotime($posts1['Published']);
+   }
+   usort($posts, 'sortByDate')
 
  ?>
