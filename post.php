750c69c1f80dee864540d20479a5a198ace430e0
Filpet96 <Fpetersson96@gmail.com>
2017-10-17 22:19:06 +0200

    Removed Concatenation.. it was useless

--- post.php
+++ post.php
@@ -1,12 +1,14 @@
 <?php
 declare(strict_types=1);
 
+
+
 $posts = [
 
 [
   'Title' => 'Back to the future',
   'Content' => 'No, I refuse to except the responsibility. Is she pretty? Don\'t say a word. Are those my clocks I hear? Thank god I found you. Listen, can you meet me at Twin Pines Mall tonight at 1:15? I\'ve made a major breakthrough, I\'ll need your assistance',
-  'Author' => 'Posted by' . ' ' . 'Morty',
+  'Author' => 'Morty',
   'Published' => '1985-06-03',
   'Likes' => '359',
   'Image' => '/images/delorean.jpg'
@@ -14,7 +16,7 @@ $posts = [
 [
   'Title' => 'The Dark Knight',
   'Content' => 'I had a vision of a world without Batman. The Mob ground out a little profit and the police tried to shut them down one block at a time. And it was so boring. I\'ve had a change of heart. I don\'t want Mr Reese spoiling everything but why should I have all the fun? Let\'s give someone else a chance. If Coleman Reese isn\'t dead in 60 minutes then I blow up a hospital.',
-  'Author' => 'Posted by' . ' ' . 'Joker',
+  'Author' => 'Joker',
   'Published' => '2008-07-13',
   'Likes' => '469',
   'Image' => 'http://cdn2.business2community.com/wp-content/uploads/2016/10/Heaths-Joker-300x128.jpg.jpg'
@@ -22,7 +24,7 @@ $posts = [
 [
   'Title' => 'Rocky V',
   'Content' => 'Hey there, don\'t punch me. I\'m gettin\' brittle as it is. Look at this, ya know? I been runnin\' up and down these steps for 20 years. And I never knew there was valuable pictures in this building, ya know.',
-  'Author' => 'Posted by' . ' ' . 'Rocky Balboa',
+  'Author' => 'Rocky Balboa',
   'Published' => '1990-11-16',
   'Likes' => '869',
   'Image' => 'https://images6.alphacoders.com/341/341558.jpg'
@@ -30,15 +32,15 @@ $posts = [
 [
   'Title' => 'Goodfellas',
   'Content' => 'See, the hardest thing for me was leaving the life. I still loved the life. And we were treated like movie stars with muscle. We had it all just for the asking. Our wives, mothers, kids, everybody rode along. I had paper bags filled with jewelry stashed in the kitchen. I had a sugar bowl full of coke next to the bed.',
-  'Author' => 'Posted by' . ' ' . 'James Conway',
+  'Author' => 'James Conway',
   'Published' => '1990-09-17',
   'Likes' => '569',
-  'Image' => 'http://cdn-static.denofgeek.com/sites/denofgeek/files/2017/02/goodfellas_poster.jpg'
+  'Image' => 'http://yesofcorsa.com/wp-content/uploads/2015/08/4559_goodfellas-1024x576.jpg'
 ],
 [
   'Title' => 'Malcolm X',
   'Content' => 'As Brother Malcolm said: \'We declare our right on this earth to be a man, to be a human being, to be given the rights of a human being, to be respected as a human being in this society on this earth in this day which we intend to bring into existence -- by any means necessary!',
-  'Author' => 'Posted by' . ' ' . 'Malcolm X',
+  'Author' => 'Malcolm X',
   'Published' => '1992-11-18',
   'Likes' => '169',
   'Image' => 'https://media2.s-nbcnews.com/i/MSNBC/Components/Video/150220/a_flashback_malcolm_150219.jpg'
