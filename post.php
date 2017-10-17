0ad2a589f5ecd57883e305bf24f98ee3d702c2b9
Filpet96 <Fpetersson96@gmail.com>
2017-10-17 19:59:15 +0200

    added 4 more posts of movies

--- post.php
+++ post.php
@@ -12,7 +12,38 @@ $posts = [
   'Likes' => '359',
   'Image' => '/images/delorean.jpg'
 ],
-
+[
+  'Title' => 'The Dark Knight',
+  'Content' => 'I had a vision of a world without Batman. The Mob ground out a little profit and the police tried to shut them down one block at a time. And it was so boring. I\'ve had a change of heart. I don\'t want Mr Reese spoiling everything but why should I have all the fun? Let\'s give someone else a chance. If Coleman Reese isn\'t dead in 60 minutes then I blow up a hospital.',
+  'Author' => 'Joker',
+  'Published' => '2008-07-13',
+  'Likes' => '469',
+  'Image' => 'http://cdn2.business2community.com/wp-content/uploads/2016/10/Heaths-Joker-300x128.jpg.jpg'
+],
+[
+  'Title' => 'Rocky V',
+  'Content' => 'Hey there, don\'t punch me. I\'m gettin\' brittle as it is. Look at this, ya know? I been runnin\' up and down these steps for 20 years. And I never knew there was valuable pictures in this building, ya know.',
+  'Author' => 'Rocky Balboa',
+  'Published' => '1990-11-16',
+  'Likes' => '869',
+  'Image' => 'https://images6.alphacoders.com/341/341558.jpg'
+],
+[
+  'Title' => 'Goodfellas',
+  'Content' => 'See, the hardest thing for me was leaving the life. I still loved the life. And we were treated like movie stars with muscle. We had it all just for the asking. Our wives, mothers, kids, everybody rode along. I had paper bags filled with jewelry stashed in the kitchen. I had a sugar bowl full of coke next to the bed.',
+  'Author' => 'James Conway',
+  'Published' => '1990-09-17',
+  'Likes' => '569',
+  'Image' => 'http://yesofcorsa.com/wp-content/uploads/2015/08/4559_goodfellas-1024x576.jpg'
+],
+[
+  'Title' => 'Malcolm X',
+  'Content' => 'As Brother Malcolm said: \'We declare our right on this earth to be a man, to be a human being, to be given the rights of a human being, to be respected as a human being in this society on this earth in this day which we intend to bring into existence -- by any means necessary!',
+  'Author' => 'Malcolm X',
+  'Published' => '1992-11-18',
+  'Likes' => '169',
+  'Image' => 'https://media2.s-nbcnews.com/i/MSNBC/Components/Video/150220/a_flashback_malcolm_150219.jpg'
+],
 
 ];
 
