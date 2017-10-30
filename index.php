be5b73d4d8d5044dd2b17e54e99a6e248dc9249e
Filpet96 <Fpetersson96@gmail.com>
2017-10-30 11:13:55 +0100

    Updated new title

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ require(__DIR__ . '/functions.php');
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title></title>
+  <title>Good-News-Project</title>
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
